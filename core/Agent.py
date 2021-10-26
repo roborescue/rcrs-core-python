ad8860b83f67780881f7cdbda4e4c2d31c74cb6d
Ali <modaresi.mr@gmail.com>
2021-10-26 10:04:56 +0200

    update to version 6

--- core/Agent.py
+++ core/Agent.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 import sys
 import traceback
 import core.RCRSProto_pb2 as RCRSProto_pb2
-import core.urn as urn
+import core.URN as URN
 class Agent(ABC):
     def __init__(self):
         pass
@@ -49,11 +49,11 @@ class Agent(ABC):
             
 
     def messageReceived(self,msg):
-        if(msg.urn==urn.ControlMSG.KA_SENSE):
+        if(msg.urn==URN.ControlMSG.KA_SENSE):
             self.handleKASense(msg)
-        elif(msg.urn==urn.ControlMSG.KA_CONNECT_OK):
+        elif(msg.urn==URN.ControlMSG.KA_CONNECT_OK):
             self.handleKAConnectOk(msg)
-        elif (msg.urn==urn.ControlMSG.KA_CONNECT_ERROR):
+        elif (msg.urn==URN.ControlMSG.KA_CONNECT_ERROR):
             self.handleKAConnectError(msg)
     
     def handleKASense(self,msg):
@@ -62,10 +62,10 @@ class Agent(ABC):
         if(self.sock.recv(1,socket.MSG_PEEK)):
             print("next cycle is recevied but you are still in the previous one")
             overtime=True
-        agentId=msg.components["Agent ID"].entityID
-        time=msg.components['Time'].intValue
-        changeSet=msg.components['Updates'].changeSet
-        hear=msg.components['Hearing'].commandList
+        agentId=msg.components[URN.ComponentControlMSG.AgentID].entityID
+        time=msg.components[URN.ComponentControlMSG.Time].intValue
+        changeSet=msg.components[URN.ComponentControlMSG.Updates].changeSet
+        hear=msg.components[URN.ComponentControlMSG.Hearing].commandList
         if(self.id!=agentId):
             print(f'ERRROR this should not never happen agentid={self.id} but receive a sense for {agentId}')
         self.world.merge(changeSet)
@@ -76,12 +76,12 @@ class Agent(ABC):
             print(f'Exception occured in think: {type(e).__name__}: {e} \n {traceback.format_exc()}')
 
     def handleKAConnectOk(self,msg):    
-        entities=msg.components["Entities"].entityList.entities
+        entities=msg.components[URN.ComponentControlMSG.Entities].entityList.entities
         self.world=WorldModel()
         self.world.addEntitiesProto(entities)
-        requestId=msg.components['Request ID'].intValue
-        self.id=msg.components["Agent ID"].entityID
-        self.config=msg.components["Agent config"].config
+        requestId=msg.components[URN.ComponentControlMSG.RequestID].intValue
+        self.id=msg.components[URN.ComponentControlMSG.AgentID].entityID
+        self.config=msg.components[URN.ComponentControlMSG.AgentConfig].config
         print(f'Connected successfully agent id={self.id}')
         try:
             self.precompute()
@@ -92,8 +92,8 @@ class Agent(ABC):
 
 
     def handleKAConnectError(self,msg):
-        requestId=msg.components['Request ID'].intValue
-        reason=msg.components['Reason'].stringValue
+        requestId=msg.components[URN.ComponentControlMSG.RequestID].intValue
+        reason=msg.components[URN.ComponentControlMSG.Reason].stringValue
         print(f'Connect Error reason={reason} requestId={requestId}')
         self.sock.close()
         import sys
@@ -101,20 +101,19 @@ class Agent(ABC):
 
     def sendAKConnect(self,requestId):
         msg=RCRSProto_pb2.MessageProto()
-        import core.urn as urn
-        msg.urn=urn.ControlMSG.AK_CONNECT
-        msg.components['Request ID'].intValue=requestId
-        msg.components['Version'].intValue=2
-        msg.components['Name'].stringValue=self.name()
+        msg.urn=URN.ControlMSG.AK_CONNECT
+        msg.components[URN.ComponentControlMSG.RequestID].intValue=requestId
+        msg.components[URN.ComponentControlMSG.Version].intValue=2
+        msg.components[URN.ComponentControlMSG.Name].stringValue=self.name()
         for urn in self.requestedEntityTypes():
-            msg.components['Requested entity types'].intList.values.append(urn)
+            msg.components[URN.ComponentControlMSG.RequestedEntityTypes].intList.values.append(urn)
         rcrs_encoding_utils.write_msg(msg,self.sock)
 
     def sendAKAcknowledge(self,requestId):
         msg=RCRSProto_pb2.MessageProto()
-        msg.urn=urn.ControlMSG.AK_ACKNOWLEDGE
-        msg.components['Request ID'].intValue=requestId
-        msg.components['Agent ID'].entityID=self.id
+        msg.urn=URN.ControlMSG.AK_ACKNOWLEDGE
+        msg.components[URN.ComponentControlMSG.RequestID].intValue=requestId
+        msg.components[URN.ComponentControlMSG.AgentID].entityID=self.id
         rcrs_encoding_utils.write_msg(msg,self.sock)
 
     def parseMessageFromKernel(self):
@@ -131,7 +130,7 @@ class Agent(ABC):
 ####################################commands
     def rest(self,time):
         msg=RCRSProto_pb2.MessageProto()
-        msg.urn=urn.Command.AK_REST
-        msg.components['Agent ID'].entityID=self.id
-        msg.components['Time'].intValue=time
+        msg.urn=URN.Command.AK_REST
+        msg.components[URN.ComponentControlMSG.AgentID].entityID=self.id
+        msg.components[URN.ComponentControlMSG.Time].intValue=time
         return msg
