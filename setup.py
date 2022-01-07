5da57660eacf3986f3113201ee40b25a44c8bb5d
Arman Absalan <armanaxh@gmail.com>
2022-01-07 16:00:43 +0330

    Fix pip install from git

--- setup.py
+++ setup.py
@@ -1,1 +1,24 @@
-print ('install i2c')
+from setuptools import setup, find_packages
+
+
+# JUST FOR TEST
+
+setup(
+    name='rcrs',
+    version='0.1.0',    
+    description='RoboCup Rescue Simulation(RCRS Agent Development Library',
+    url='https://github.com/roborescue/rcrs-core-python',
+    packages=find_packages(),
+    # install_requires=['mpi4py>=2.0',
+    #                   'numpy',                     
+    #                   ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
