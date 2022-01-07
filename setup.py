a3f82dce58006c0a3b124579cce859d472370493
ffaraji <faraji.farshid@gmail.com>
2022-01-07 23:52:24 +0330

    Change folder name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 # JUST FOR TEST
 
 setup(
-    name='rcrs-core',
+    name='rcrs_core',
     version='0.1.0',    
     description='RoboCup Rescue Simulation(RCRS Agent Development Library)',
     url='https://github.com/roborescue/rcrs-core-python',
